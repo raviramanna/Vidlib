7164f8756d2cef501d0be3deab022fb76f525976
raviramanna <raviramanna@hotmail.com.au>
2017-07-24 20:09:57 +1000

    Display Customer Birthday on their profile page

--- Vidlib/Models/Customer.cs
+++ Vidlib/Models/Customer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Web;
 
 namespace Vidlib.Models
@@ -15,5 +16,6 @@ namespace Vidlib.Models
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
+        public DateTime? BirthDate { get; set; }
     }
 }
