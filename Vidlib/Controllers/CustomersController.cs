0560c0b0d429999b2d42dd9d4971e8510b5d147b
raviramanna <raviramanna@hotmail.com.au>
2017-07-26 17:56:44 +1000

    Add a form to Add and Edit customer details

--- Vidlib/Controllers/CustomersController.cs
+++ Vidlib/Controllers/CustomersController.cs
@@ -4,6 +4,7 @@ using System.Web;
 using System.Web.Mvc;
 using Vidlib.Models;
 using System.Data.Entity;
+using Vidlib.ViewModel;
 
 namespace Vidlib.Controllers
 {
@@ -19,6 +20,37 @@ namespace Vidlib.Controllers
         protected override void Dispose(bool disposing)
         {
             _context.Dispose();
+        }
+
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormViewModel();
+            viewModel.MembershipTypes = membershipTypes;
+            return View("CustomerForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return View("CustomerForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Customers");
         }
 
         // GET: Customers
---
+++ Vidlib/ViewModel/CustomerFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidlib.Models;
+
+namespace Vidlib.ViewModel
+{
+    public class CustomerFormViewModel
+    {
+        public List<MembershipType> MembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
